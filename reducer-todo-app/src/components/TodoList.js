12342befd69e0e78debdbe56009d2e23ed64e140
fiopwk <flopwiki@gmail.com>
2020-02-26 03:31:39 -0500

    added components folder and setup state in component

---
+++ reducer-todo-app/src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { useState, useReducer } from 'react';
+
+import {initialState, simpleReducer } from '../reducers/reducer';
+
+const TodoList = () => {
+
+    return (
+        <>
+        </>
+
+    )
+}
