394f2b4c4f8e889f5e166ace8f41ecdd4cbd0061
fiopwk <flopwiki@gmail.com>
2020-02-26 09:05:09 -0500

    added input and button elements to component

--- reducer-todo-app/src/components/TodoList.js
+++ reducer-todo-app/src/components/TodoList.js
@@ -1,12 +1,34 @@
 import React, { useState, useReducer } from 'react';
+import { Input, Button } from 'reactstrap';
 
 import {initialState, simpleReducer } from '../reducers/reducer';
 
 const TodoList = () => {
 
+    const [todoList, setTodoList] = useState('');
+
+    const [state, dispatch] = useReducer(simpleReducer, initialState)
+
+    console.log(state)
+
     return (
-        <>
-        </>
+        <React.Fragment>
+            <Input 
+            type="text"
+            
+            >
+            </Input>
+
+            <Button>Add Todo</Button>
+
+            <Button>Clear completed</Button>
+
+
+        </React.Fragment>
+
+        
 
     )
 }
+
+export default TodoList;
