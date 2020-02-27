7949cb9e4698425ce543e071c020fdaf865312e7
fiopwk <flopwiki@gmail.com>
2020-02-27 15:44:38 -0500

    added comments and worked out MVP

--- reducer-todo-app/src/components/TodoList.js
+++ reducer-todo-app/src/components/TodoList.js
@@ -4,6 +4,7 @@ import { Button, Form, Input } from 'reactstrap';
 
 import '../components/TodoList.css';
 
+// Step 1: Set an initial state value through a reducer and render a list of todos from that state
 function TodoList() {
     const [todo, setTodo] = useState('');
     const [{ todos }, dispatch] = useReducer(simpleReducer, { todos: [] });
@@ -26,7 +27,9 @@ function TodoList() {
                     >
                     </Input>
                 </Form>
+                {/* Step 2: Add a form that dispatches an "ADD_TODO" action to be able to add a todo item to your list */}
                 <Button onClick={() => dispatch({ type: 'add', todo, action: setTodo('') })}>Add</Button>
+                {/* Step 4: Build a function that let's you clear completed todos when you click on a "Clear Completed" button */}
                 <Button onClick={() => dispatch({ type: 'delete', todo })}>Remove</Button>
             </div>
 
@@ -34,6 +37,7 @@ function TodoList() {
       {JSON.stringify(todos, null, 2)}
       </pre> */}
 
+            {/* Step 3: Build a function that let's you mark todo items as completed when you click on them */}
             {todos.map((t, idx) => (<div
                 className={t.completed ? 'completed' : ''}
                 key={idx}
